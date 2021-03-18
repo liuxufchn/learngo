cc8e5709acd19614c0d1870ad413ac85148ec733
shaffer <liuxufchn@gmail.com>
2021-03-18 13:20:25 +0800

    hello

---
+++ branch.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+func printTextFile() {
+	const filename = "hello/basic.go"
+	if content, err := ioutil.ReadFile(filename); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%s\n", content)
+	}
+}
+
+func convert2Binary(n int) {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lastBinary := n % 2
+		result = strconv.Itoa(lastBinary) + result
+	}
+	fmt.Println("result = ", result)
+}
+
+func printTextFileUsingBufio() {
+	const filename = "hello/basic.go"
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(fmt.Sprintf("fail to open file: %s\n", filename))
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func main() {
+	printTextFile()
+	convert2Binary(10)
+	convert2Binary(6)
+	printTextFileUsingBufio()
+}
---
+++ hello/basic.go
@@ -0,0 +1,71 @@
+package hello
+
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
+
+var (
+	packageVariable = 10
+	packageString   = "abc"
+)
+
+func variableZeroValue() {
+	var a int
+	var s string
+	fmt.Printf("%d %q\n", a, s)
+}
+
+func variableInitialValue() {
+	var a, b int = 3, 4
+	var s string = "abc"
+	fmt.Println(a, b, s)
+}
+
+func variableTypeDeduction() {
+	var a, b, c, s = 3, 4, true, "def"
+	fmt.Println(a, b, c, s)
+}
+
+func variableShorter() {
+	a, b, c, s := 3, 4, false, "def"
+	b = 5
+	fmt.Print(a, b, c, s)
+
+}
+
+func euler() {
+	fmt.Println(cmplx.Pow(math.E, 1i*math.Pi) + 1)
+}
+
+func triangle() {
+	var a, b int = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
+}
+
+func enums() {
+	const (
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+	fmt.Println(b, kb, mb, gb, tb, pb)
+}
+
+func main01() {
+	fmt.Println("hello,world")
+	variableZeroValue()
+	variableInitialValue()
+	variableTypeDeduction()
+	variableShorter()
+	fmt.Println(packageVariable, packageString)
+	euler()
+	triangle()
+	enums()
+}
